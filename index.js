1d4d9aed0d1f9041307c003bf089c7ddcd1bf009
AE SChan <abigailsinchan@gmail.com>
2023-06-23 22:59:42 -0400

    fixed weather api

--- index.js
+++ index.js
@@ -1,83 +1,114 @@
+// Get references to the necessary HTML elements
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('city-input');
+const cityNameElement = document.getElementById('city-name');
+const currentDateElement = document.getElementById('current-date');
+const currentTemperatureElement = document.getElementById('current-temperature');
+const currentHumidityElement = document.getElementById('current-humidity');
+const currentWindSpeedElement = document.getElementById('current-wind-speed');
+const currentIconElement = document.getElementById('current-icon');
+const forecastItemsElement = document.getElementById('forecast-items');
 const apiKey = 'ef3834312c73da95dcd5c0744aacfc9b';
 
-const searchForm = document.getElementById('search-form');
-searchForm.addEventListener('submit', handleFormSubmit);
+// Event listener for form submission
+searchForm.addEventListener('submit', function (e) {
+  e.preventDefault(); // Prevent form submission
 
-function handleFormSubmit(event) {
-    event.preventDefault();
+  const city = cityInput.value.trim();
 
-    const cityInput = document.getElementById('city-input');
-    const cityName = cityInput.value.trim();
+  // Call the geocoding API to get latitude and longitude for the city
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length > 0) {
+        const { lat, lon } = data[0];
 
-    if (cityName !== '') {
-        fetchWeatherData(cityName);
-    } else {
-        alert('Please enter a city name.');
-    }
+        // Call the current weather API to get the current weather data
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Update the current weather elements with the retrieved data
+            cityNameElement.textContent = city;
+            currentDateElement.textContent = getCurrentDate();
+            // Convert temperature from Kelvin to Fahrenheit
+            const currentTemperatureFahrenheit = Math.round(
+                ((data.main.temp - 273.15) * 9) / 5 + 32);
+            currentTemperatureElement.textContent = `${currentTemperatureFahrenheit}°F`;
+            currentHumidityElement.textContent = `Humidity: ${data.main.humidity}%`;
+            currentWindSpeedElement.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+            currentIconElement.setAttribute(
+              'src',
+              `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+            );
+            currentIconElement.setAttribute('alt', data.weather[0].description);
+          })
+          .catch((error) => {
+            console.log('Error fetching current weather:', error);
+          });
 
-    cityInput.value = '';
-}
+        // Call the 5-day forecast API to get the forecast data
+        fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Clear previous forecast items
+            forecastItemsElement.innerHTML = '';
 
-function fetchWeatherData(cityName) {
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.length > 0) {
-                const { lat, lon } = data[0];
-                fetchWeatherByCoordinates(lat, lon);
-            } else {
-                console.log('No results found for the given city name.');
+            // Loop through the forecast data and create forecast items for each day
+            for (let i = 0; i < data.list.length; i += 8) {
+              const forecast = data.list[i];
+
+              const forecastItem = document.createElement('div');
+              forecastItem.classList.add('forecast-item');
+
+              const forecastDate = document.createElement('p');
+              forecastDate.textContent = formatDate(forecast.dt);
+              forecastItem.appendChild(forecastDate);
+
+            // Convert temperature from Kelvin to Fahrenheit
+            const forecastTemperatureFahrenheit = Math.round(
+                ((forecast.main.temp - 273.15) * 9) / 5 + 32);
+             forecastTemperature.textContent = `${forecastTemperatureFahrenheit}°F`;
+              forecastItem.appendChild(forecastTemperature);
+
+              const forecastIcon = document.createElement('img');
+              forecastIcon.setAttribute(
+                'src',
+                `http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`
+              );
+              forecastIcon.setAttribute('alt', forecast.weather[0].description);
+              forecastItem.appendChild(forecastIcon);
+
+              forecastItemsElement.appendChild(forecastItem);
             }
-        })
-        .catch(error => {
-            console.log('Error:', error);
-        });
-}
-
-function fetchWeatherByCoordinates(latitude, longitude) {
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&units=metric&appid=${apiKey}`)
-        .then(response => response.json())
-        .then(data => {
-            const city = data.timezone.split('/').pop(); // Extract the city name from the timezone
-            const currentWeather = data.current;
-            const dailyWeather = data.daily;
-
-            updateWeatherUI(city, currentWeather, dailyWeather);
-        })
-        .catch(error => {
-            console.log('Error:', error);
-        });
-}
-
-function updateWeatherUI(city, currentWeather, dailyWeather) {
-    const cityNameElement = document.getElementById('city-name');
-    const currentDateElement = document.getElementById('current-date');
-    const currentTemperatureElement = document.getElementById('current-temperature');
-    const currentHumidityElement = document.getElementById('current-humidity');
-    const currentWindSpeedElement = document.getElementById('current-wind-speed');
-    const currentIconElement = document.getElementById('current-icon');
-    const forecastItemsElement = document.getElementById('forecast-items');
-
-    cityNameElement.textContent = city;
-    currentDateElement.textContent = `Date: ${new Date(currentWeather.dt * 1000).toLocaleDateString()}`;
-    currentTemperatureElement.textContent = `Temperature: ${currentWeather.temp}°C`;
-    currentHumidityElement.textContent = `Humidity: ${currentWeather.humidity}%`;
-    currentWindSpeedElement.textContent = `Wind Speed: ${currentWeather.wind_speed} m/s`;
-    currentIconElement.src = `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}.png`;
-    currentIconElement.alt = 'Weather Icon';
-
-    forecastItemsElement.innerHTML = '';
-
-    dailyWeather.forEach(day => {
-        const forecastItem = document.createElement('div');
-        forecastItem.classList.add('forecastItem');
-        forecastItem.innerHTML = `
-            <p>Date: ${new Date(day.dt * 1000).toLocaleDateString()}</p>
-            <p>Temperature: ${day.temp.day}°C</p>
-            <p>Humidity: ${day.humidity}%</p>
-            <p>Wind Speed: ${day.wind_speed} m/s</p>
-            <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}.png" alt="Weather Icon">
-        `;
-        forecastItemsElement.appendChild(forecastItem);
+          })
+          .catch((error) => {
+            console.log('Error fetching forecast:', error);
+          });
+      } else {
+        console.log('City not found');
+      }
+    })
+    .catch((error) => {
+      console.log('Error fetching geocoding:', error);
     });
+});
+
+// Function to get the current date in the format "Day, Month Date"
+function getCurrentDate() {
+  const options = { weekday: 'long', month: 'long', day: 'numeric' };
+  const currentDate = new Date();
+  return currentDate.toLocaleDateString(undefined, options);
+}
+
+// Function to format the forecast date
+function formatDate(timestamp) {
+  const options = { weekday: 'short', month: 'short', day: 'numeric' };
+  const forecastDate = new Date(timestamp * 1000);
+  return forecastDate.toLocaleDateString(undefined, options);
 }
